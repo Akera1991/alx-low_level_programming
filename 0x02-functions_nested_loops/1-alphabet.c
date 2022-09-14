44704d481d30c149434ef73de5d28e9fef170fa7
Akera1991 <akeraemmy2014@gmail.com>
2022-09-14 14:05:05 -0700

    commitng

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main-programs
+ *
+ * Return :returns 0
+ */
+void print_alphabet(void)
+{
+
+	int alp = 'a';
+
+	while (alp<='z')
+	{
+		_putchar(alph);
+		alp++;
+	}
+	putchar('\n');
+}
