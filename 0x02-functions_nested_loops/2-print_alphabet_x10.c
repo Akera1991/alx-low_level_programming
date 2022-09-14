9a902bba106e114b4d415ccaa7cea672ecb3bc52
Akera1991 <akeraemmy2014@gmail.com>
2022-09-14 14:47:06 -0700

    commiting

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * printing alpha_10x
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i=0;
+while (i<10)
+{
+in alph ='a';
+while (alph<='z')
+
+{
+_putchar(alph);
+alph++;
+}
+_putchar('\n')
+i++;
+}}
