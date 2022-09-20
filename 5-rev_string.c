1c9117ceaff57e8343d8dead5c2e726cb9aba3fe
Akera1991 <akeraemmy2014@gmail.com>
2022-09-20 13:58:29 -0700

    commiting

---
+++ 5-rev_string.c
@@ -0,0 +1,19 @@
+/**
+ * rev_string -reverse a string
+ *
+ * @s: string paremeter input
+ *
+ * Return :Nothing
+ */
+void rev_string(char *5)
+	int 1, i;
+	char ch;
+	/* find string lenth without null char*/
+	for (1=0; s[1]!='\0'; ++);
+	/swap the string by looping to half the string*/
+	for (i=0; i< 1/2; ++1)
+	  ch=s[i]
+	  s[i]=s[1-1-i];
+	  s[1-1-i]=ch;
+	  }
+}
